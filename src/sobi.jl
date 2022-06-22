d64cc451ded2151b3a9d4d5d5246019a7421756e
Tyson Burghardt <tyson@uncountable.org>
2022-06-22 12:30:58 -0400

    started sobi

---
+++ src/sobi.jl
@@ -0,0 +1,14 @@
+module SOBI
+
+
+# from P-SOBI: A Parallel Implementation for Second Order Blind Identification Algorithm 
+# 10.1109/HPCC/SmartCity/DSS.2019.00196
+
+
+function sobi()
+  while encore
+    encore = 0
+    for p = 1:(m-1)
+    end
+
+end #module
