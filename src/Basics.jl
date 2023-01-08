9c5ab0620f214a608d2ef042aabc483aa59b412c
Tyson Burghardt <41615+psammetichus@users.noreply.github.com>
2023-01-08 03:42:56 -0500

    implemented spectral edge frequency function

--- src/Basics.jl
+++ src/Basics.jl
@@ -6,4 +6,32 @@ Signal = Vector{Float64}
 
 RMat = Array{Float64,2}
 
+
+spectralEdge(s :: Signal, Fs :: Float64, k=0.95, overlap :: Int64, width :: Int64, windower)
+    ll = length(s)
+    lls = zeros(ll - overlap)
+    while i < ll-overlap
+        segment = windower(s[i:i+width])
+        ffs = rfft(segment)
+        ssum = 0.0
+        total = sum( abs.(ffs).^2 )
+        edge = k*total
+        for j in 1..div(width,2)+1
+            ssum += abs(ffs[j])^2
+            if ssum >= edge
+                lls[i] = (j/(div(width,2)+1) * div(Fs, 2))
+            end
+        end
+        i += overlap
+    end
+    return lls
+end
+
+
+
+
+
+    
+
+
 end #module
