ffb3371c2db1efbbaa157d42b96628e8d2724f69
Tyson Burghardt <tyson@uncountable.org>
2023-02-23 16:52:26 -0500

    eegframe

--- src/EEGData.jl
+++ src/EEGData.jl
@@ -6,15 +6,25 @@ struct Annotation
 end #Annotation struct
   
 struct EEG
-  signals :: Dict{String, Vector{Float64}}
+  signals :: Dict{String, Signal}
   Fs :: Float64
   annots :: Vector{Annotation}
 end #EEG struct
 
-function signalDataFrame(eeg :: EEG)
-  DataFrame(eeg.signals)
+struct EEGFrame
+  signals :: DataFrame
+  annots :: DataFrame
+  Fs :: Float64
 end
 
-function getSignal(fromTime :: Float64, toTime :: Float64)
-  
+function convertToDataFrame(eeg :: EEG) :: EEGFrame
+  ss = DataFrame(eeg.signals)
+  aa = DataFrame(eeg.annots)
+  EEGFrame(ss,aa,eeg.Fs)
+end
+
+function getSignal(eeg:: EEGFrame, fromTime :: Float64, toTime :: Float64)
+  @assert f<t
+  f, t = Int.(floor.((fromTime, toTime).*eeg.Fs))
+  eeg.signals[f:t, :]
 end #function
