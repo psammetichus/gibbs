8fd6764d7a5905400f4d1a2f5b466e18ea0a6401
Tyson Burghardt <tyson@uncountable.org>
2023-02-23 16:20:31 -0500

    added loadJLD

--- src/fileio/eegJLD.jl
+++ src/fileio/eegJLD.jl
@@ -1,3 +1,7 @@
 function saveAsJLD(filename :: String, eeg :: EEG)
     JLD.save(filename, "eeg", eeg)
 end #saveAsJLD
+
+function loadJLD(filename :: String)
+    JLD.load(filename, "eeg")
+end #loadJLD
