4d076718eedc015fe09e7702a26fb91743c031fa
Tyson Burghardt <tyson@uncountable.org>
2024-01-02 19:27:33 -0500

    filter done

--- src/filter.jl
+++ src/filter.jl
@@ -9,16 +9,26 @@ end
 
 function dataFirFilter(data, bpass, taps=64, Fs)
     zpg = prepareFilter(bpass, taps, Fs)
-    fData = filter(data, zpg)
+    return filt(data, zpg)
 end
 
-function eegFirFilter(eeg :: EEG, bpass, taps=64)
+function eegFirFilter!(eeg :: EEG, bpass, taps=64)
     for trode, sig in eeg.signals
         eeg.signals[trode] = dataFirFilter(sig, bpass, taps, eeg.Fs)
     end
 end
 
 function dataIirFilter(data, bpass, order=2, Fs)
-    
+    l,h = bpass
+    zpg = analogfilter(Bandpass(l,h; Fs=Fs), Butterworth(order/2))
+    return filt(data, zpg)
+end
+
+function eegIirFilter!(eeg :: EEG, bpass)
+    for trode, sig in eeg.signals
+        eeg.signals[trode] = dataIirFilter(sig, bpass, eeg.Fs)
+    end
+end
+
 
 end #module
