7ee630d2a87a9717af0824ab1ed7219a45145064
Tyson Burghardt <tb@inquiet.us>
2024-01-17 11:44:50 -0500

    work on sql file format

--- src/fileio/eegfile.sql
+++ src/fileio/eegfile.sql
@@ -9,7 +9,23 @@ create table Chunks (
 
 create table RecordData (
       recordId integer primary key autoincrement,
-      age integer,
       fs real
+);
+
+create table Subject (
+      subjID integer primary key autoincrement,
+      age integer,
+      gender text,
+);
+
+create table RecordXSubject (
+      RelID integer primary key autoincrement,
+      subj integer references Subject(subjID) on delete cascade,
+      record integer references RecordData(recordID) on delete cascade
+);
+
+create table Electrodes (
+      trodeID integer primary key autoincrement,
+      trodeName text
 );
 
