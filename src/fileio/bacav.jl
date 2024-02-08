e3495dafbe9e422284943fee9c93d1811fe619c3
Tyson Clark Burghardt <burghar8@msu.edu>
2024-02-08 10:23:10 -0500

    bacav export function

---
+++ src/fileio/bacav.jl
@@ -0,0 +1,10 @@
+function exportTSV(filename :: String, eeg :: EEG, signame1, signame2)
+   sig1 = getSignal(eeg, signame1)
+   sig2 = getSignal(eeg, signame2)
+   f = open(filename, "w")
+   for i in 1:eeg.length
+     println("$sig1[i] $sig2[i]")
+   end #for
+   close(f)
+end #function
+
