fb1b13aa51de37b53cc2d89d70d8e5510781a726
Tyson Clark Burghardt <burghar8@msu.edu>
2024-02-09 10:23:30 -0500

    added filtering to bacav export function

--- src/fileio/bacav.jl
+++ src/fileio/bacav.jl
@@ -1,6 +1,10 @@
 function exportBacAv(filename :: String, eeg :: EEG, signame, emgname="EMG")
    eegsig = getSignalAvg(eeg, signame)
    emgsig = getSignalDiff(eeg, emgname*"1", emgname*"2")
+   eegsig = eegFirFilter(eegsig, (1.0, 70.0), eeg.Fs)
+   emgsig = eegFirFilter(emgsig, (10.0, 200.0), eeg.Fs)
+   eegsig = notch(eegsig, eeg.Fs)
+   emgsig = notch(emgsig, eeg.Fs)
    f = open(filename, "w")
    for i in 1:eeg.length
      s = eegsig[i]
