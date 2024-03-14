4ab0fe2e8dbb16d4b73d9e4411a4774051ffc73b
Tyson Burghardt <tyson@uncountable.org>
2024-03-14 16:31:38 -0400

    docstrings for average.jl

--- src/basics/average.jl
+++ src/basics/average.jl
@@ -1,11 +1,12 @@
-avgTrodes = ["Fp1", "Fp2", "F7", "F8", "T7", "T8", "P7", "P8",
+const avgTrodes = ["Fp1", "Fp2", "F7", "F8", "T7", "T8", "P7", "P8",
             "O1", "O2", "F3", "F4", "C3", "C4", "P3", "P4", "Fz", "Cz", "Pz"]
 
-function makeAvgRef(eeg)
+"creates a signal based on `eeg` which represents the average of the list `avgtrodes`"
+function makeAvgRef(eeg :: EEG)
   ntrodes = length(avgTrodes)
   avgsig = zeros(eeg.length)
   sigs = zeros(eeg.length, ntrodes)
-  for (j,trode) ∈ enumerate(avgTrodes)
+  for (j,trode) ∈ enumerate(global avgTrodes)
     sigs[:,j] = getSignal(eeg,trode)
   end #for
   for i in 1:eeg.length
@@ -15,7 +16,8 @@ function makeAvgRef(eeg)
   return avgsig
 end #function
 
-function makeAvgRef!(eeg)
+"re-references all the signals in `eeg` to the average"
+function makeAvgRef!(eeg :: EEG)
   ntrodes = length(avgTrodes)
   avgRef = makeAvgRef(eeg)
   for t in avgTrodes
