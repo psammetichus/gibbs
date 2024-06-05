e25eb794ebd5ac3879c4177a4feb375609055a11
Tyson Clark Burghardt <burghar8@msu.edu>
2024-06-05 09:55:09 -0400

    removed windowing from spectraledge

--- src/analysis/misc.jl
+++ src/analysis/misc.jl
@@ -1,26 +1,19 @@
 """
-calculates the vector of spectral edge frequencies parameterized by `k` of a signal windowed by `windower` with given
-width and overlap.
+calculates the vector of spectral edge frequencies parameterized by `k` of a signal 
 """
-function spectralEdge(s :: Vector{Float64}, Fs :: Float64, overlap :: Int64, width :: Int64, windower, k=0.95)
+function spectralEdge(s :: Vector{Float64}, Fs :: Float64, k=0.95)
     ll = length(s)
-    lls = zeros(ll - (width - overlap))
-    i = 1
-    while i < ll - (width - overlap)
-        segment = windower(s[i:i+width])
-        ffs = rfft(segment)
-        ssum = 0.0
-        total = sum( abs.(ffs).^2 )
-        edge = k*total
-        for j in 1:div(width,2)+1
-            ssum += abs(ffs[j])^2
-            if ssum >= edge
-                lls[i] = (j/(div(width,2)+1) * div(Fs, 2))
-            end
+    ffs = rfft(s)
+    ssum = 0.0
+    total = sum( abs.(ffs).^2 )
+    edge = k*total
+    for j in 1:div(width,2)+1
+        ssum += abs(ffs[j])^2
+        if ssum >= edge
+            return (j/(div(width,2)+1) * div(Fs, 2))
         end
-        i += overlap
     end
-    return lls
+    return Fs/2
 end #func
 
 # line length functions from Line length: An efficient feature for seizure onset detection, Esteller et al, 2001
