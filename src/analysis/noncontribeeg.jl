fd633ad20a63552e5632c5e468618a2d37fca95a
Tyson Burghardt <41615+psammetichus@users.noreply.github.com>
2024-09-07 15:27:32 -0400

    outline of a method for estimating epilepsy based on noncontributory EEG

---
+++ src/analysis/noncontribeeg.jl
@@ -0,0 +1,23 @@
+#taken from Estimating the likelihood of epilepsy from clinically noncontributory electroencephalograms using computational analysis: A retrospective, multisite case–control study by Tait et al. 2024
+#
+#
+
+
+function estNoncontribEEG(eeg)
+
+    #calc peak α freq at O1 and O2 
+    #calc high α power = relative α (10.5-13.5) averaged across channels after
+    #bipolar referencing frontal and temporal channels
+    #
+    #calc low α (6-9) phase locking value as adjacency matrix of undirected
+    #graph
+    #calc mean degree
+    #calc degree variance
+    #calc avg weighted clustering coeff
+    #calc characteristic path length
+    #
+    #calc global coupling
+    #calc local coupling
+    #calc critical coupling
+
+end
